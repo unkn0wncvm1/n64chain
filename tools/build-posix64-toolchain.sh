f43cf1810b64c2ced38c0a3a6b190517f4b4fd64
unkn0wncvm1 <87480097+unkn0wncvm1@users.noreply.github.com>
2025-03-05 16:29:32 +0300

    Update build-posix64-toolchain.sh

--- tools/build-posix64-toolchain.sh
+++ tools/build-posix64-toolchain.sh
@@ -19,9 +19,9 @@ which getconf >/dev/null 2>/dev/null && {
 
 numproc=`getnumproc`
 
-BINUTILS="ftp://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.bz2"
-GCC="ftp://ftp.gnu.org/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.gz"
-MAKE="ftp://ftp.gnu.org/gnu/make/make-4.3.tar.gz"
+BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.bz2"
+GCC="https://ftp.gnu.org/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.gz"
+MAKE="https://ftp.gnu.org/gnu/make/make-4.3.tar.gz"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR} && mkdir -p {stamps,tarballs}
